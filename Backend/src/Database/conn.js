a3de07f28e265cb01dd419351062a82e5891d7e0
dreisdev <dreis.dev@outlook.com>
2023-12-28 17:18:46 -0300

    fix config database.

--- Backend/src/Database/conn.js
+++ Backend/src/Database/conn.js
@@ -5,15 +5,12 @@ const mongoose = require("mongoose");
 const Connection = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://project_mao_amiga:${process.env.DATA_BASE_PASS}@datadreis.dnoi4fg.mongodb.net/?retryWrites=true&w=majority`,
-      {
-        bufferCommands: false,
-        connectTimeoutMS: 30000,
-      }
+      `mongodb+srv://project_mao_amiga:${process.env.DATA_BASE_PASS}@datadreis.dnoi4fg.mongodb.net/?retryWrites=true&w=majority`
     );
     console.log("Connected to database");
   } catch (error) {
     console.log(`Error connecting to database: ${error}`);
+    throw error;
   }
 };
 
