5f40a88914797cf090c9feb22a8b7ea9308623ad
dreisdev <dreis.dev@outlook.com>
2023-12-30 04:33:00 -0300

    config api.

--- src/components/Stripe/Payment_Boleto.jsx
+++ src/components/Stripe/Payment_Boleto.jsx
@@ -2,212 +2,206 @@ import "./Payment_Boleto.css";
 
 import { useState } from "react";
 
-import { useStripe } from '@stripe/react-stripe-js';
-import axios from "axios";
+import { useStripe } from "@stripe/react-stripe-js";
 import CircularIndeterminate from "../Progress/Progress";
-import { StyledEngineProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from "@mui/material/styles";
 import { NavLink } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileLines } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileLines } from "@fortawesome/free-solid-svg-icons";
+import api from "../../api/fetchApi";
 
 const DonationFormBoleto = () => {
-    const stripe = useStripe();
+  const stripe = useStripe();
 
-    const [messageError, setMessageError] = useState("");
-    const [country, setCountry] = useState('BR');
-    const [state, setState] = useState('');
-    const [city, setCity] = useState('');
-    const [postalCode, setPostalCode] = useState('');
-    const [line1, setLine1] = useState('');
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [taxId, setTaxId] = useState('');
-    const [amountBoleto, setAmountBoleto] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [resultError, setResultError] = useState(false);
-    const [resultPayment, setResultPayment] = useState(false);
+  const [messageError, setMessageError] = useState("");
+  const [country, setCountry] = useState("BR");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [line1, setLine1] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [taxId, setTaxId] = useState("");
+  const [amountBoleto, setAmountBoleto] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [resultError, setResultError] = useState(false);
+  const [resultPayment, setResultPayment] = useState(false);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        setLoading(true);
+    setLoading(true);
 
-        try {
+    try {
+      const amountCent = amountBoleto * 100;
 
-            const amountCent = amountBoleto * 100;
+      let clientSecret;
 
+      const response = await api.post("/payment/boleto", {
+        amountBoleto: amountCent,
+      });
 
-            let clientSecret;
+      if (response && response.data && response.data.client_secret) {
+        clientSecret = response.data.client_secret;
 
-            const response = await axios.post('https://server-maoamiga-api.cyclic.app/payment/boleto', {
-                amountBoleto: amountCent,
-            });
+        const result = await stripe.confirmBoletoPayment(clientSecret, {
+          payment_method: {
+            boleto: {
+              tax_id: taxId,
+            },
+            billing_details: {
+              name,
+              email,
+              address: {
+                line1,
+                city,
+                state,
+                postal_code: postalCode,
+                country,
+              },
+            },
+          },
+        });
 
+        if (result.error) {
+          console.error(result.error);
 
-            if (response && response.data && response.data.client_secret) {
-                clientSecret = response.data.client_secret;
+          const errorMessage = result.error.message || "Erro desconhecido";
+          setMessageError(`Error: ${errorMessage}`);
 
-
-                const result = await stripe.confirmBoletoPayment(clientSecret, {
-                    payment_method: {
-                        boleto: {
-                            tax_id: taxId,
-                        },
-                        billing_details: {
-                            name,
-                            email,
-                            address: {
-                                line1,
-                                city,
-                                state,
-                                postal_code: postalCode,
-                                country,
-                            },
-                        },
-                    },
-                });
-
-
-                if (result.error) {
-                    console.error(result.error);
-
-                    const errorMessage = result.error.message || 'Erro desconhecido';
-                    setMessageError(`Error: ${errorMessage}`);
-
-                    setResultError(true);
-                    setLoading(false);
-                } else {
-                    setLoading(false);
-                    setResultError(false);
-                    setResultPayment(true);
-                    setState('');
-                    setCity('');
-                    setPostalCode('');
-                    setLine1('');
-                    setName('');
-                    setEmail('');
-                    setTaxId('');
-                    setAmountBoleto('--');
-
-                }
-            }
-
-
-        } catch (error) {
-            console.error('Erro ao fazer a solicitação:', error.message);
-            setResultError(true);
-            setLoading(false);
+          setResultError(true);
+          setLoading(false);
+        } else {
+          setLoading(false);
+          setResultError(false);
+          setResultPayment(true);
+          setState("");
+          setCity("");
+          setPostalCode("");
+          setLine1("");
+          setName("");
+          setEmail("");
+          setTaxId("");
+          setAmountBoleto("--");
         }
+      }
+    } catch (error) {
+      console.error("Erro ao fazer a solicitação:", error.message);
+      setResultError(true);
+      setLoading(false);
+    }
+  };
 
-    };
+  return (
+    <>
+      <section className="payment-boleto">
+        <h1>
+          <FontAwesomeIcon icon={faFileLines} /> Boleto
+        </h1>
 
+        <form id="payment-form" onSubmit={handleSubmit}>
+          <label className="payment-label" htmlFor="name">
+            Nome:
+          </label>
+          <input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
 
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-    return (
-        <>
+          <label htmlFor="taxId">CPF ou CNPJ</label>
+          <input
+            id="taxId"
+            value={taxId}
+            onChange={(e) => setTaxId(e.target.value)}
+            required
+          />
 
-            <section className="payment-boleto">
-                <h1><FontAwesomeIcon icon={faFileLines} /> Boleto</h1>
+          <label htmlFor="country">País (BR)</label>
+          <input
+            id="country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            required
+          />
 
-                <form id="payment-form" onSubmit={handleSubmit}>
-                    <label className="payment-label" htmlFor="name">Nome:</label>
-                    <input
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
+          <label htmlFor="state">Digite a UF</label>
+          <input
+            id="state"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            required
+          />
 
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        id="email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
+          <label htmlFor="city">Cidade</label>
+          <input
+            id="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
 
-                    <label htmlFor="taxId">CPF ou CNPJ</label>
-                    <input
-                        id="taxId"
-                        value={taxId}
-                        onChange={(e) => setTaxId(e.target.value)}
-                        required
-                    />
+          <label htmlFor="postal_code">Cep (Somente números)</label>
+          <input
+            id="postal_code"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+            required
+          />
 
-                    <label htmlFor="country">País (BR)</label>
-                    <input
-                        id="country"
-                        value={country}
-                        onChange={(e) => setCountry(e.target.value)}
-                        required
-                    />
+          <label htmlFor="line1">Endereço</label>
+          <input
+            id="line1"
+            value={line1}
+            onChange={(e) => setLine1(e.target.value)}
+            required
+          />
 
-                    <label htmlFor="state">Digite a UF</label>
-                    <input
-                        id="state"
-                        value={state}
-                        onChange={(e) => setState(e.target.value)}
-                        required
-                    />
+          <label htmlFor="amountBoleto">Digite o Valor: R$</label>
+          <input
+            id="amountBoleto"
+            value={amountBoleto}
+            onChange={(e) => setAmountBoleto(e.target.value)}
+            required
+          />
 
-                    <label htmlFor="city">Cidade</label>
-                    <input
-                        id="city"
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                        required
-                    />
+          {loading ? (
+            <div className="loading">
+              <StyledEngineProvider injectFirst>
+                <CircularIndeterminate message="Aguarde, boleto sendo emitido..." />
+              </StyledEngineProvider>
+            </div>
+          ) : resultError ? (
+            <div>
+              Ocorreu um erro. Por favor, revise os campos, e tente novamente.
+              {messageError}
+            </div>
+          ) : resultPayment ? (
+            <button className="btn-back-home">
+              <NavLink className="style-btn-back-home" to="/">
+                Obrigado sua Doação foi registrada - Voltar para Página Inicial
+              </NavLink>
+            </button>
+          ) : null}
 
-                    <label htmlFor="postal_code">Cep (Somente números)</label>
-                    <input
-                        id="postal_code"
-                        value={postalCode}
-                        onChange={(e) => setPostalCode(e.target.value)}
-                        required
-                    />
-
-                    <label htmlFor="line1">Endereço</label>
-                    <input
-                        id="line1"
-                        value={line1}
-                        onChange={(e) => setLine1(e.target.value)}
-                        required
-                    />
-
-                    <label htmlFor="amountBoleto">Digite o Valor: R$</label>
-                    <input
-                        id="amountBoleto"
-                        value={amountBoleto}
-                        onChange={(e) => setAmountBoleto(e.target.value)}
-                        required
-                    />
-
-
-                    {loading ? (
-                        <div className="loading">
-                            <StyledEngineProvider injectFirst>
-                                <CircularIndeterminate message="Aguarde, boleto sendo emitido..." />
-                            </StyledEngineProvider>
-                        </div>
-                    ) : resultError ? (
-                        <div>Ocorreu um erro. Por favor, revise os campos, e tente novamente.{messageError}</div>
-                    ) : resultPayment ? (
-                        <button className="btn-back-home" >
-                            <NavLink className="style-btn-back-home" to="/">Obrigado sua Doação foi registrada - Voltar para Página Inicial</NavLink>
-                        </button>
-                    ) : null}
-
-                    <button className="btn-pay" type="submit">
-                        Gerar Boleto
-                    </button>
-
-                </form>
-
-            </section>
-        </>
-    );
+          <button className="btn-pay" type="submit">
+            Gerar Boleto
+          </button>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default DonationFormBoleto;
--- src/components/Stripe/Payment_Card.jsx
+++ src/components/Stripe/Payment_Card.jsx
@@ -2,163 +2,150 @@ import "./Payment_Card.css";
 
 import { useState } from "react";
 
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import axios from "axios";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import CircularIndeterminate from "../Progress/Progress";
-import { StyledEngineProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from "@mui/material/styles";
 import { NavLink } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreditCard } from "@fortawesome/free-solid-svg-icons";
+import api from "../../api/fetchApi";
 
 const DonationFormCard = () => {
-    const stripe = useStripe();
-    const elements = useElements();
+  const stripe = useStripe();
+  const elements = useElements();
 
-    const [nameCustomer, setNameCustomer] = useState("");
-    const [messageError, setMessageError] = useState("");
-    const [amountCard, setAmountCard] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [resultError, setResultError] = useState(false);
-    const [resultPayment, setResultPayment] = useState(false);
+  const [nameCustomer, setNameCustomer] = useState("");
+  const [messageError, setMessageError] = useState("");
+  const [amountCard, setAmountCard] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [resultError, setResultError] = useState(false);
+  const [resultPayment, setResultPayment] = useState(false);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        setLoading(true);
+    setLoading(true);
 
-        try {
+    try {
+      const amountCent = amountCard * 100;
 
-            const amountCent = amountCard * 100;
+      let clientSecret;
 
+      const response = await api.post("/payment/card", {
+        amountCard: amountCent,
+      });
 
-            let clientSecret;
+      if (response && response.data && response.data.client_secret) {
+        clientSecret = response.data.client_secret;
 
-            const response = await axios.post('https://server-maoamiga-api.cyclic.app/payment/card', {
-                amountCard: amountCent,
-            });
+        const result = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: {
+            card: elements.getElement(CardElement),
+            billing_details: {
+              name: nameCustomer,
+            },
+          },
+        });
 
+        if (result.error) {
+          const errorMessage = result.error.message || "Erro desconhecido";
 
-            if (response && response.data && response.data.client_secret) {
-                clientSecret = response.data.client_secret;
-
-
-                const result = await stripe.confirmCardPayment(clientSecret, {
-                    payment_method: {
-                        card: elements.getElement(CardElement),
-                        billing_details: {
-                            name: nameCustomer,
-
-                        },
-                    },
-                },
-                );
-
-
-                if (result.error) {
-
-                    const errorMessage = result.error.message || 'Erro desconhecido';
-
-
-                    console.error(result.error);
-                    setResultError(true);
-                    setMessageError(`Error: ${errorMessage}`);
-                    setLoading(false);
-                } else {
-                    setLoading(false);
-                    setResultError(false);
-                    setResultPayment(true);
-                    setNameCustomer('');
-                    setAmountCard('--');
-
-
-                }
-            }
-
-
-        } catch (error) {
-            console.error('Erro ao fazer a solicitação:', error.message);
-            setResultError(true);
-            setMessageError(error.message);
-            setLoading(false);
+          console.error(result.error);
+          setResultError(true);
+          setMessageError(`Error: ${errorMessage}`);
+          setLoading(false);
+        } else {
+          setLoading(false);
+          setResultError(false);
+          setResultPayment(true);
+          setNameCustomer("");
+          setAmountCard("--");
         }
+      }
+    } catch (error) {
+      console.error("Erro ao fazer a solicitação:", error.message);
+      setResultError(true);
+      setMessageError(error.message);
+      setLoading(false);
+    }
+  };
 
-    };
+  return (
+    <>
+      <section className="payment-boleto">
+        <h1 className="payment-card-title">
+          <FontAwesomeIcon icon={faCreditCard} /> Cartão de Crédito
+        </h1>
 
+        <form id="payment-form" onSubmit={handleSubmit}>
+          <label className="payment-label" htmlFor="card">
+            Digite os Dados do Cartão:
+          </label>
 
+          <div className="element-card">
+            <CardElement
+              id="card"
+              className="element-card-content"
+              options={{
+                style: {
+                  base: {
+                    fontSize: "14px",
+                    color: "#424770",
+                    "::placeholder": {
+                      color: "#aab7c4",
+                    },
+                  },
+                  invalid: {
+                    color: "#9e2146",
+                  },
+                },
+              }}
+            />
+          </div>
 
-    return (
-        <>
+          <label htmlFor="nameCustomer">Digite seu nome:</label>
+          <input
+            id="nameCustomer"
+            value={nameCustomer}
+            onChange={(e) => setNameCustomer(e.target.value)}
+            required
+          />
 
-            <section className="payment-boleto" >
-                <h1 className="payment-card-title"><FontAwesomeIcon icon={faCreditCard} /> Cartão de Crédito</h1>
+          <label htmlFor="amountCard">Digite o Valor: R$</label>
+          <input
+            id="amountCard"
+            value={amountCard}
+            onChange={(e) => setAmountCard(e.target.value)}
+            required
+          />
 
-                <form id="payment-form" onSubmit={handleSubmit}>
-                    <label className="payment-label" htmlFor="card">Digite os Dados do Cartão:</label>
+          {loading ? (
+            <div className="loading">
+              <StyledEngineProvider injectFirst>
+                <CircularIndeterminate message="Processando Pagamento" />
+              </StyledEngineProvider>
+            </div>
+          ) : resultError ? (
+            <div>
+              Ocorreu um erro. Por favor, revise os campos, e tente novamente.
+              {messageError}
+            </div>
+          ) : resultPayment ? (
+            <button className="btn-back-home">
+              <NavLink className="style-btn-back-home" to="/">
+                Obrigado sua Doação foi registrada - Voltar para Página Inicial
+              </NavLink>
+            </button>
+          ) : null}
 
-                    <div className="element-card">
-                        <CardElement id="card" className="element-card-content"
-
-                            options={{
-                                style: {
-                                    base: {
-                                        fontSize: '14px',
-                                        color: '#424770',
-                                        '::placeholder': {
-                                            color: '#aab7c4',
-                                        },
-                                    },
-                                    invalid: {
-                                        color: '#9e2146',
-                                    },
-                                },
-                            }}
-
-                        />
-                    </div>
-
-                    <label htmlFor="nameCustomer">Digite seu nome:</label>
-                    <input
-                        id="nameCustomer"
-                        value={nameCustomer}
-                        onChange={(e) => setNameCustomer(e.target.value)}
-                        required
-                    />
-
-
-
-                    <label htmlFor="amountCard">Digite o Valor: R$</label>
-                    <input
-                        id="amountCard"
-                        value={amountCard}
-                        onChange={(e) => setAmountCard(e.target.value)}
-                        required
-                    />
-
-
-                    {loading ? (
-                        <div className="loading">
-                            <StyledEngineProvider injectFirst>
-                                <CircularIndeterminate message="Processando Pagamento" />
-                            </StyledEngineProvider>
-                        </div>
-                    ) : resultError ? (
-                        <div>Ocorreu um erro. Por favor, revise os campos, e tente novamente.{messageError}</div>
-                    ) : resultPayment ? (
-                        <button className="btn-back-home" >
-                            <NavLink className="style-btn-back-home" to="/">Obrigado sua Doação foi registrada - Voltar para Página Inicial</NavLink>
-                        </button>
-                    ) : null}
-
-                    <button className="btn-pay" type="submit">
-                        Pagar
-                    </button>
-
-                </form>
-
-            </section>
-        </>
-    );
+          <button className="btn-pay" type="submit">
+            Pagar
+          </button>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default DonationFormCard;
