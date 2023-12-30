39de6ff14bd287c236fbcac506f72c6063b4d4c3
dreisdev <dreis.dev@outlook.com>
2023-12-30 04:34:01 -0300

    general config

--- src/components/ConfirmationModal/ConfirmationModal.jsx
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 import "./ConfirmationModal.css";
 
+import { useAdmin } from "../../Context/AdminContext";
+
 const ConfirmationModal = ({ message, onConfirm, onCancel }) => {
+  const { projectTrue, eventTrue } = useAdmin();
   return (
     <div className="confirmation-modal">
       <p>{message}</p>
-      <button onClick={onConfirm}>Excluir Evento</button>
+      <button onClick={onConfirm}>
+        Excluir {projectTrue && !eventTrue ? "Projeto" : "Evento"}
+      </button>
       <button onClick={onCancel}>Não Excluir</button>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ import Gallery from "./pages/Gallery/gallery";
 import Events from "./pages/Events/events";
 import Contacts from "./pages/Contacts/contacts";
 import Admin from "./pages/Adm/panelAdm";
+import AdminProject from "./pages/AdmProjects/panelAdmProjects";
 import Login from "./pages/Login/login";
 
 import { loadStripe } from "@stripe/stripe-js";
@@ -30,13 +31,11 @@ import PaymentBoleto from "./components/Stripe/Payment_Boleto.jsx";
 import PaymentCard from "./components/Stripe/Payment_Card.jsx";
 import PaymentPix from "./components/Stripe/Payment_Pix.jsx";
 
-import axios from "axios";
-
 import ProtectedRoute from "./routes/protectedRoute.jsx";
+import { AdminProvider } from "./Context/AdminContext.jsx";
+import api from "./api/fetchApi.jsx";
 
-const response = await axios.get(
-  "https://server-maoamiga-api.cyclic.app/config"
-);
+const response = await api.get("/config");
 
 const stripePromisse = loadStripe(`${response.data.publishableKey}`);
 
@@ -95,6 +94,31 @@ const router = createBrowserRouter([
       },
 
       {
+        path: "/adm/project/*",
+        element: (
+          <ProtectedRoute redirectTo="/login">
+            <AdminProject />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/adm/editproject/:id",
+        element: (
+          <ProtectedRoute redirectTo="/login">
+            <AdminProject />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/adm/deleteproject/:id/*",
+        element: (
+          <ProtectedRoute redirectTo="/login">
+            <AdminProject />
+          </ProtectedRoute>
+        ),
+      },
+
+      {
         path: "/login",
         element: <Login />,
       },
@@ -126,6 +150,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AdminProvider>
+      <RouterProvider router={router} />
+    </AdminProvider>
   </React.StrictMode>
 );
